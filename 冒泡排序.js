fb020928d90cedbee4e5d031b5ae1a9f34945b6f
Amywang1205 <myself2258@126.com>
2019-02-01 14:13:36 +0800

    Create 冒泡排序.js

---
+++ 冒泡排序.js
@@ -0,0 +1,19 @@
+function bubbleSort (arr) {
+  let max = arr.length - 1;
+  for (let i = 0; i < max; i++) {
+    // 声明一个变量，作为标志位
+    let done = true;
+    for (let j = 0; j < max - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        done = false;
+      }
+    }
+    if (done) {
+      break;
+    }
+  }
+  return arr;
+}
